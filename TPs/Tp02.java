cfa51a2514ab5af21d41a333b5e975634fc49fd0
Fernando Rossi <jfrjunior@sga.pucminas.br>
2022-10-28 17:09:35 -0300

    Geração randomica de grafos

--- TPs/Tp02.java
+++ TPs/Tp02.java
@@ -14,41 +14,31 @@ public class Tp02 {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         Integer nVertices = 10;
-        Boolean save = false;
-        String path = "D:\\graphs\\Data\\graph-random-eurelian-" + nVertices + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices, 2, 4, save, "eurelian");
-        path = "D:\\graphs\\Data\\graph-random-eurelian-" + nVertices * 10 + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices * 10, 2, 20, save, "eurelian");
-        path = "D:\\graphs\\Data\\graph-random-eurelian-" + nVertices * 100 + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices * 100, 2, 30, save, "eurelian");
-        path = "D:\\graphs\\Data\\graph-random-eurelian-" + nVertices * 1000 + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices * 1000, 2, 50, save, "eurelian");
-        path = "D:\\graphs\\Data\\graph-random-eurelian-" + nVertices * 10000 + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices * 10000, 2, 70, save, "eurelian");
+        Boolean save = true;
+        String basePath = "Data\\";
+        String eurelianPath = basePath + "eurelian\\graph-random-eurelian-{nVertices}.txt";
+        String semiEurelianPath = basePath + "semi-eurelian\\graph-random-semi_eurelian-{nVertices}.txt";
+        String notEurelianPath = basePath + "not-eurelian\\graph-random-not_eurelian-{nVertices}.txt";
+        for(int i=0; i<5; i++) {
+            GraphsUtils.generateRandomGraph(eurelianPath.replace("{nVertices}", String.valueOf(nVertices)+"("+((i+1))+")"), nVertices, 2, 4, save, "eurelian");
+            GraphsUtils.generateRandomGraph(eurelianPath.replace("{nVertices}", String.valueOf(nVertices*10)+"("+(i+1)+")"), nVertices * 10, 2, 20, save, "eurelian");
+            GraphsUtils.generateRandomGraph(eurelianPath.replace("{nVertices}", String.valueOf(nVertices*100)+"("+(i+1)+")"), nVertices * 100, 2, 30, save, "eurelian");
+            GraphsUtils.generateRandomGraph(eurelianPath.replace("{nVertices}", String.valueOf(nVertices*1000)+"("+(i+1)+")"), nVertices * 1000, 2, 50, save, "eurelian");
+            GraphsUtils.generateRandomGraph(eurelianPath.replace("{nVertices}", String.valueOf(nVertices*10000)+"("+(i+1)+")"), nVertices * 10000, 2, 70, save, "eurelian");
+
+            GraphsUtils.generateRandomGraph(semiEurelianPath.replace("{nVertices}", String.valueOf(nVertices)+"("+(i+1)+")"), nVertices, 2, 4, save, "semi-eurelian");
+            GraphsUtils.generateRandomGraph(semiEurelianPath.replace("{nVertices}", String.valueOf(nVertices*10)+"("+(i+1)+")"), nVertices * 10, 2, 20, save, "semi-eurelian");
+            GraphsUtils.generateRandomGraph(semiEurelianPath.replace("{nVertices}", String.valueOf(nVertices*100)+"("+(i+1)+")"), nVertices * 100, 2, 30, save, "semi-eurelian");
+            GraphsUtils.generateRandomGraph(semiEurelianPath.replace("{nVertices}", String.valueOf(nVertices*1000)+"("+(i+1)+")"), nVertices * 1000, 2, 50, save, "semi-eurelian");
+            GraphsUtils.generateRandomGraph(semiEurelianPath.replace("{nVertices}", String.valueOf(nVertices*10000)+"("+(i+1)+")"), nVertices * 10000, 2, 70, save, "semi-eurelian");
 
 
-        path = "D:\\graphs\\Data\\graph-random-semi_eurelian-" + nVertices + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices, 2, 4, save, "semi-eurelian");
-        path = "D:\\graphs\\Data\\graph-random-semi_eurelian-" + nVertices * 10 + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices * 10, 2, 20, save, "semi-eurelian");
-        path = "D:\\graphs\\Data\\graph-random-semi_eurelian-" + nVertices * 100 + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices * 100, 2, 30, save, "semi-eurelian");
-        path = "D:\\graphs\\Data\\graph-random-semi_eurelian-" + nVertices * 1000 + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices * 1000, 2, 50, save, "semi-eurelian");
-        path = "D:\\graphs\\Data\\graph-random-semi_eurelian-" + nVertices * 10000 + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices * 10000, 2, 70, save, "semi-eurelian");
-
-
-        path = "D:\\graphs\\Data\\graph-random-not_eurelian-" + nVertices + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices, 2, 4, save, "not-eurelian");
-        path = "D:\\graphs\\Data\\graph-random-not_eurelian-" + nVertices * 10 + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices * 10, 2, 20, save, "not-eurelian");
-        path = "D:\\graphs\\Data\\graph-random-not_eurelian-" + nVertices * 100 + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices * 100, 2, 30, save, "not-eurelian");
-        path = "D:\\graphs\\Data\\graph-random-not_eurelian-" + nVertices * 1000 + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices * 1000, 2, 50, save, "not-eurelian");
-        path = "D:\\graphs\\Data\\graph-random-not_eurelian-" + nVertices * 10000 + ".txt";
-        GraphsUtils.generateRandomGraph(path, nVertices * 10000, 2, 70, save, "not-eurelian");
+            GraphsUtils.generateRandomGraph(notEurelianPath.replace("{nVertices}", String.valueOf(nVertices)+"("+(i+1)+")"), nVertices, 2, 4, save, "not-eurelian");
+            GraphsUtils.generateRandomGraph(notEurelianPath.replace("{nVertices}", String.valueOf(nVertices*10)+"("+(i+1)+")"), nVertices * 10, 2, 20, save, "not-eurelian");
+            GraphsUtils.generateRandomGraph(notEurelianPath.replace("{nVertices}", String.valueOf(nVertices*100)+"("+(i+1)+")"), nVertices * 100, 2, 30, save, "not-eurelian");
+            GraphsUtils.generateRandomGraph(notEurelianPath.replace("{nVertices}", String.valueOf(nVertices*1000)+"("+(i+1)+")"), nVertices * 1000, 2, 50, save, "not-eurelian");
+            GraphsUtils.generateRandomGraph(notEurelianPath.replace("{nVertices}", String.valueOf(nVertices*10000)+"("+(i+1)+")"), nVertices * 10000, 2, 70, save, "not-eurelian");
+        }
         sc.close();
     }
 }
