f7ff6c0c5ba6032ef2925fe26390ce2890c60425
Fernando Rossi <jfrjunior@sga.pucminas.br>
2022-10-30 00:51:09 -0300

    Fleury (contém muito lixo)

--- Model/NotDirectedGraph.java
+++ Model/NotDirectedGraph.java
@@ -2,11 +2,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Stream;
 
 public class NotDirectedGraph {
     private List<List<Integer>> edges;
     private List<List<Integer>> edgesClone;
+
+    // Depth search variables
+    private Integer[] tDiscovery;
+    private Integer[] tFinish;
+    private Integer[] father;
+
+    private List<Integer[]> edgesEurelianTour;
 
     public NotDirectedGraph(String path) throws IOException {
         this.edges = GraphsUtils.processFileNotDirected(new BufferedReader(new FileReader(path)));
@@ -21,25 +27,76 @@ public class NotDirectedGraph {
         return getSuccessors(vertice).size();
     }
 
-    private Integer depthSearch(Integer time, Integer currentVertice,
-                                Integer[] tDiscovery, Integer[] tFinish, Integer[] father) {
+    public List<List<Integer>> getConnectedComponents() {
+        List<List<Integer>> components = new ArrayList<>();
+        tDiscovery = new Integer[this.edges.size()];
+        this.tDiscovery = Arrays.stream(this.tDiscovery).map(j -> j = 0).toArray(Integer[]::new);
+        int i = 0;
+        while (Arrays.stream(this.tDiscovery).anyMatch(x -> x == 0)) {
+            components.add(new ArrayList<>());
+            Integer root = null;
+            for (int j = 0; j < this.edges.size(); j++) {
+                if (this.tDiscovery[j] == 0) {
+                    root = j;
+                    break;
+                }
+            }
+            depthSearchIterative(root);
+            for (int j = 0; j < this.edges.size(); j++) {
+                if (tDiscovery[j] == 1) {
+                    components.get(i).add(j);
+                    tDiscovery[j] = -1;
+                }
+            }
+            i++;
+        }
+        return components;
+    }
+
+    public Boolean isConnected() {
+        tDiscovery = new Integer[this.edges.size()];
+        this.tDiscovery = Arrays.stream(this.tDiscovery).map(j -> j = 0).toArray(Integer[]::new);
+        depthSearchIterative(0);
+        return Arrays.stream(this.tDiscovery).noneMatch(x -> x == 0);
+
+    }
+
+    private void depthSearchIterative(Integer root) {
+        Stack nodes = new Stack();
+        getSuccessors(root + 1).stream().sorted(Comparator.naturalOrder()).forEach(nodes::push);
+        while (!nodes.isEmpty()) {
+            Integer current = (Integer) nodes.pop();
+            if (tDiscovery[current] == 0) {
+                tDiscovery[current] = 1;
+                getSuccessors(current + 1).stream().sorted(Comparator.naturalOrder()).forEach(nodes::push);
+            }
+        }
+
+    }
+
+    private Integer depthSearchRecursive(Integer time, Integer currentVertice) {
         time++;
-        tDiscovery[currentVertice] = time;
+        this.tDiscovery[currentVertice] = time;
         for (Integer edge : this.edgesClone.get(currentVertice).stream().sorted(Comparator.naturalOrder()).toList()) {
-            if (tDiscovery[edge] == 0)
-                time = depthSearch(time, edge, tDiscovery, tFinish, father);
+            if (this.tDiscovery[edge] == 0)
+                time = depthSearchRecursive(time, edge);
         }
         time++;
-        tFinish[currentVertice] = time;
+        this.tFinish[currentVertice] = time;
 
         return time;
     }
-    
-    private void removeEdge(Integer vertice1, Integer vertice2, List<List<Integer>> graph){
+
+    private void removeEdge(Integer vertice1, Integer vertice2) {
+        removeEdge(vertice1, vertice2, this.edges);
+    }
+
+    private void removeEdge(Integer vertice1, Integer vertice2, List<List<Integer>> graph) {
         graph.get(vertice1).remove(vertice2);
         graph.get(vertice2).remove(vertice1);
     }
-    private void addEdge(Integer vertice1, Integer vertice2, List<List<Integer>> graph){
+
+    private void addEdge(Integer vertice1, Integer vertice2, List<List<Integer>> graph) {
         graph.get(vertice1).add(vertice2);
         graph.get(vertice2).add(vertice1);
     }
@@ -65,11 +122,11 @@ public class NotDirectedGraph {
                 father = Arrays.stream(father).map(j -> j = 0).toArray(Integer[]::new);
                 Integer time = 0;
                 Integer currentVertice = tDiscovery[0];
-                time = depthSearch(time, currentVertice, tDiscovery, tFinish, father);
+                time = depthSearchRecursive(time, currentVertice);
 
                 //Checking unexplored vertices
-                if(Arrays.stream(tDiscovery).anyMatch(x -> x==0)){
-                    if(!bridges.containsKey(i))
+                if (Arrays.stream(tDiscovery).anyMatch(x -> x == 0)) {
+                    if (!bridges.containsKey(i))
                         bridges.put(i, new ArrayList<>());
                     bridges.get(i).add(e);
                 }
@@ -77,5 +134,79 @@ public class NotDirectedGraph {
             }
         }
         return bridges;
+    }
+
+    public void tarjan() {
+        List<List<Integer>> components = getConnectedComponents();
+
+    }
+
+    public List<Integer[]> eurelianTour() {
+        edgesEurelianTour = new ArrayList<>();
+        Integer u = 0;
+        for (int i = 0; i < this.edges.size(); i++) {
+            if (getOutDegree(i+1) % 2 == 1) {
+                u = i;
+                break;
+            }
+        }
+        eurelianUtil(u);
+        return this.edgesEurelianTour;
+    }
+
+    private void eurelianUtil(Integer u) {
+        // Recur for all the vertices adjacent to this
+        // vertex
+        for (int i = 0; i < getOutDegree(u+1); i++) {
+            Integer v = getSuccessors(u+1).get(i);
+            // If edge u-v is a valid next edge
+            if (isValidNextEdge(u, v)) {
+                this.edgesEurelianTour.add(new Integer[]{u+1, v+1});
+                // This edge is used so remove it now
+                removeEdge(u, v, this.edgesClone);
+                eurelianUtil(v);
+            }
+        }
+    }
+
+    private boolean isValidNextEdge(Integer u, Integer v) {
+        // The edge u-v is valid in one of the
+        // following two cases:
+
+        // 1) If v is the only adjacent vertex of u
+        // ie size of adjacent vertex list is 1
+        if (getOutDegree(u+1) == 1) {
+            return true;
+        }
+
+        // 2) If there are multiple adjacents, then
+        // u-v is not a bridge Do following steps
+        // to check if u-v is a bridge
+        // 2.a) count of vertices reachable from u
+        boolean[] isVisited = new boolean[this.edges.size()];
+        int count1 = dfsCount(u, isVisited);
+
+        // 2.b) Remove edge (u, v) and after removing
+        //  the edge, count vertices reachable from u
+        removeEdge(u, v);
+        isVisited = new boolean[this.edges.size()];
+        int count2 = dfsCount(u, isVisited);
+
+        // 2.c) Add the edge back to the graph
+        addEdge(u, v, this.edgesClone);
+        return count1 <= count2;
+    }
+
+    private int dfsCount(Integer v, boolean[] isVisited) {
+        // Mark the current node as visited
+        isVisited[v] = true;
+        int count = 1;
+        // Recur for all vertices adjacent to this vertex
+        for (int adj : getSuccessors(v+1)) {
+            if (!isVisited[adj]) {
+                count = count + dfsCount(adj, isVisited);
+            }
+        }
+        return count;
     }
 }
--- TPs/Tp02.java
+++ TPs/Tp02.java
@@ -1,43 +1,60 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Tp02 {
+    static String basePath = "Data\\";
+    static String eurelianPath = basePath + "eurelian\\graph-random-eurelian-{nVertices}{index}.txt";
+    static String semiEurelianPath = basePath + "semi-eurelian\\graph-random-semi_eurelian-{nVertices}{index}.txt";
+    static String notEurelianPath = basePath + "not-eurelian\\graph-random-not_eurelian-{nVertices}{index}.txt";
     public static void main1(String[] args) throws IOException {
-        String path = "D:\\graphs\\Data\\graph-test-small.txt";
+        String path = "D:\\graphs\\Data\\others\\graph-test-small.txt";
         NotDirectedGraph ndg = new NotDirectedGraph(path);
         System.out.println(ndg.naive());
     }
+    public static void main(String[] args) throws IOException{
+        List<NotDirectedGraph> graphs = new ArrayList<>();
+        for(int i=1; i<=5; i++)
+            try {
+                new NotDirectedGraph((replace(eurelianPath, 10, i))).eurelianTour().forEach(j -> System.out.print(j[0] + " - " + j[1] + " | "));
+//                System.out.println(new NotDirectedGraph("D:\\graphs\\Data\\others\\graph-eurelian-tour-test.txt").getConnectedComponents() + " | " + i);
+//                new NotDirectedGraph("D:\\graphs\\Data\\others\\graph-eurelian-tour-test.txt").eurelianTour().forEach(j -> System.out.print(j[0] + " - " + j[1] + " | "));
+                System.out.println();
+            } catch (StackOverflowError e){
+                i--;
+//                e.printStackTrace();
+            }
+            }
+    private static String replace(String path, Integer nVertices, Integer index){
+        return path.replace("{nVertices}", String.valueOf(nVertices))
+                .replace("{index}", "("+((index))+")");
+    }
 
-    public static void main(String[] args) throws IOException {
+    public static void main2(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         Integer nVertices = 10;
-        Boolean save = true;
-        String basePath = "Data\\";
-        String eurelianPath = basePath + "eurelian\\graph-random-eurelian-{nVertices}.txt";
-        String semiEurelianPath = basePath + "semi-eurelian\\graph-random-semi_eurelian-{nVertices}.txt";
-        String notEurelianPath = basePath + "not-eurelian\\graph-random-not_eurelian-{nVertices}.txt";
+        Boolean save = false;
+
         for(int i=0; i<5; i++) {
-            GraphsUtils.generateRandomGraph(eurelianPath.replace("{nVertices}", String.valueOf(nVertices)+"("+((i+1))+")"), nVertices, 2, 4, save, "eurelian");
-            GraphsUtils.generateRandomGraph(eurelianPath.replace("{nVertices}", String.valueOf(nVertices*10)+"("+(i+1)+")"), nVertices * 10, 2, 20, save, "eurelian");
-            GraphsUtils.generateRandomGraph(eurelianPath.replace("{nVertices}", String.valueOf(nVertices*100)+"("+(i+1)+")"), nVertices * 100, 2, 30, save, "eurelian");
-            GraphsUtils.generateRandomGraph(eurelianPath.replace("{nVertices}", String.valueOf(nVertices*1000)+"("+(i+1)+")"), nVertices * 1000, 2, 50, save, "eurelian");
-            GraphsUtils.generateRandomGraph(eurelianPath.replace("{nVertices}", String.valueOf(nVertices*10000)+"("+(i+1)+")"), nVertices * 10000, 2, 70, save, "eurelian");
+            GraphsUtils.generateRandomGraph(replace(eurelianPath, nVertices, i+1), nVertices, 2, 4, save, "eurelian");
+            GraphsUtils.generateRandomGraph(replace(eurelianPath, nVertices*10, i+1), nVertices * 10, 2, 20, save, "eurelian");
+            GraphsUtils.generateRandomGraph(replace(eurelianPath, nVertices*100, i+1), nVertices * 100, 2, 30, save, "eurelian");
+            GraphsUtils.generateRandomGraph(replace(eurelianPath, nVertices*1000, i+1), nVertices * 1000, 2, 50, save, "eurelian");
+            GraphsUtils.generateRandomGraph(replace(eurelianPath, nVertices*10000, i+1), nVertices * 10000, 2, 70, save, "eurelian");
 
-            GraphsUtils.generateRandomGraph(semiEurelianPath.replace("{nVertices}", String.valueOf(nVertices)+"("+(i+1)+")"), nVertices, 2, 4, save, "semi-eurelian");
-            GraphsUtils.generateRandomGraph(semiEurelianPath.replace("{nVertices}", String.valueOf(nVertices*10)+"("+(i+1)+")"), nVertices * 10, 2, 20, save, "semi-eurelian");
-            GraphsUtils.generateRandomGraph(semiEurelianPath.replace("{nVertices}", String.valueOf(nVertices*100)+"("+(i+1)+")"), nVertices * 100, 2, 30, save, "semi-eurelian");
-            GraphsUtils.generateRandomGraph(semiEurelianPath.replace("{nVertices}", String.valueOf(nVertices*1000)+"("+(i+1)+")"), nVertices * 1000, 2, 50, save, "semi-eurelian");
-            GraphsUtils.generateRandomGraph(semiEurelianPath.replace("{nVertices}", String.valueOf(nVertices*10000)+"("+(i+1)+")"), nVertices * 10000, 2, 70, save, "semi-eurelian");
+            GraphsUtils.generateRandomGraph(replace(semiEurelianPath, nVertices, i+1), nVertices, 2, 4, save, "semi-eurelian");
+            GraphsUtils.generateRandomGraph(replace(semiEurelianPath, nVertices*10, i+1), nVertices * 10, 2, 20, save, "semi-eurelian");
+            GraphsUtils.generateRandomGraph(replace(semiEurelianPath, nVertices*100, i+1), nVertices * 100, 2, 30, save, "semi-eurelian");
+            GraphsUtils.generateRandomGraph(replace(semiEurelianPath, nVertices*1000, i+1), nVertices * 1000, 2, 50, save, "semi-eurelian");
+            GraphsUtils.generateRandomGraph(replace(semiEurelianPath, nVertices*10000, i+1), nVertices * 10000, 2, 70, save, "semi-eurelian");
 
 
-            GraphsUtils.generateRandomGraph(notEurelianPath.replace("{nVertices}", String.valueOf(nVertices)+"("+(i+1)+")"), nVertices, 2, 4, save, "not-eurelian");
-            GraphsUtils.generateRandomGraph(notEurelianPath.replace("{nVertices}", String.valueOf(nVertices*10)+"("+(i+1)+")"), nVertices * 10, 2, 20, save, "not-eurelian");
-            GraphsUtils.generateRandomGraph(notEurelianPath.replace("{nVertices}", String.valueOf(nVertices*100)+"("+(i+1)+")"), nVertices * 100, 2, 30, save, "not-eurelian");
-            GraphsUtils.generateRandomGraph(notEurelianPath.replace("{nVertices}", String.valueOf(nVertices*1000)+"("+(i+1)+")"), nVertices * 1000, 2, 50, save, "not-eurelian");
-            GraphsUtils.generateRandomGraph(notEurelianPath.replace("{nVertices}", String.valueOf(nVertices*10000)+"("+(i+1)+")"), nVertices * 10000, 2, 70, save, "not-eurelian");
+            GraphsUtils.generateRandomGraph(replace(notEurelianPath, nVertices, i+1), nVertices, 2, 4, save, "not-eurelian");
+            GraphsUtils.generateRandomGraph(replace(notEurelianPath, nVertices*10, i+1), nVertices * 10, 2, 20, save, "not-eurelian");
+            GraphsUtils.generateRandomGraph(replace(notEurelianPath, nVertices*100, i+1), nVertices * 100, 2, 30, save, "not-eurelian");
+            GraphsUtils.generateRandomGraph(replace(notEurelianPath, nVertices*1000, i+1), nVertices * 1000, 2, 50, save, "not-eurelian");
+            GraphsUtils.generateRandomGraph(replace(notEurelianPath, nVertices*10000, i+1), nVertices * 10000, 2, 70, save, "not-eurelian");
         }
         sc.close();
     }
